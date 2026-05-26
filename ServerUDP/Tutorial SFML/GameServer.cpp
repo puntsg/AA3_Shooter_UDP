532565e45e866f68dee2826ff8aa60dceb19279d
Edgar Novas <edgar.novas@enti.cat>
2026-05-26 02:02:32 +0200

    GameServer for UDP
    Taken the protocolData of the other project

---
+++ ServerUDP/Tutorial SFML/GameServer.cpp
@@ -0,0 +1,202 @@
+#include "GameServer.h"
+#include <iostream>
+#include <chrono>
+
+GameServer::GameServer()
+    : pool(MAX_THREADS)
+    , running(false)
+{
+}
+
+GameServer::~GameServer()
+{
+    Stop();
+}
+
+bool GameServer::Start()
+{
+    if (udpSocket.bind(UDP_GAME_PORT) != sf::Socket::Status::Done)
+    {
+        std::cerr << "Fail bind UDP " << UDP_GAME_PORT << std::endl;
+        return false;
+    }
+    udpSocket.setBlocking(true);
+
+    if (tcpListener.listen(TCP_LISTEN_PORT) != sf::Socket::Status::Done)
+    {
+        std::cerr << "Fail listen TCP " << TCP_LISTEN_PORT << std::endl;
+        return false;
+    }
+
+    running = true;
+
+    std::cout << "TCP ON: " << TCP_LISTEN_PORT
+              << " UDP ON: " << UDP_GAME_PORT
+              << " Threads Num: " << pool.Size()
+              << std::endl;
+
+    tcpThread = std::thread(&GameServer::TcpListenerLoop, this);
+    udpThread = std::thread(&GameServer::UdpReceiveLoop,  this);
+    updateThread = std::thread(&GameServer::UpdateLoop,   this);
+
+    return true;
+}
+
+void GameServer::Stop()
+{
+    running = false;
+    udpSocket.unbind();
+    tcpListener.close();
+
+    if (tcpThread.joinable()) tcpThread.join();
+    if (udpThread.joinable()) udpThread.join();
+    if (updateThread.joinable()) updateThread.join();
+}
+
+void GameServer::TcpListenerLoop()
+{
+    tcpListener.setBlocking(true);
+
+    while (running)
+    {
+        sf::TcpSocket bootstrapSocket;
+        if (tcpListener.accept(bootstrapSocket) != sf::Socket::Status::Done)
+            continue;
+
+        std::cout << "TCP link gotten" << std::endl;
+
+        sf::Packet packet;
+        if (bootstrapSocket.receive(packet) == sf::Socket::Status::Done)
+        {
+            PacketType type = NONE;
+            packet >> type;
+
+            if (type == START_GAME)
+                HandleSessionStart(packet);
+        }
+
+        bootstrapSocket.disconnect();
+    }
+}
+
+void GameServer::UdpReceiveLoop()
+{
+    while (running)
+    {
+        sf::Packet packet;
+        std::optional<sf::IpAddress> senderIp;
+        unsigned short senderPort;
+
+        if (udpSocket.receive(packet, senderIp, senderPort) == sf::Socket::Status::Done)
+        {
+            if (!senderIp.has_value())
+                continue;
+
+            sf::IpAddress ip = senderIp.value();
+
+            // metemos el packet a procesar
+            pool.Enqueue([this, ip, senderPort, packet]() mutable
+            {
+                RouteUdpPacket(ip, senderPort, packet);
+            });
+        }
+    }
+}
+
+void GameServer::UpdateLoop()
+{
+    sf::Clock clock;
+
+    while (running)
+    {
+        float dt = clock.restart().asSeconds();
+
+        {
+            std::lock_guard<std::mutex> lock(sessionsMutex);
+            for (std::pair<const std::string, std::shared_ptr<GameSession>>& pair : sessions)
+                pair.second->Update(dt);
+
+            CleanFinishedSessions();
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
+    }
+}
+
+void GameServer::HandleSessionStart(sf::Packet& packet)
+{
+    SessionStartData sessionData;
+    packet >> sessionData;
+
+    if (sessionData.players.size() < 2)
+        return;
+
+    std::shared_ptr<GameSession> session = std::make_shared<GameSession>(
+        sessionData.roomId,
+        sessionData.players[0],
+        sessionData.players[1],
+        udpSocket
+    );
+
+    {
+        std::lock_guard<std::mutex> lock(sessionsMutex);
+        sessions[sessionData.roomId] = session;
+    }
+
+    std::cout << "Room created: " << sessionData.roomId
+              << " Total rooms: " << sessions.size() << std::endl;
+}
+
+void GameServer::RouteUdpPacket(const sf::IpAddress& senderIp, unsigned short senderPort, sf::Packet& packet)
+{
+    PacketType type = NONE;
+    packet >> type;
+
+    std::shared_ptr<GameSession> session = nullptr;
+    int playerId = -1;
+
+    {
+        std::lock_guard<std::mutex> lock(sessionsMutex);
+        for (std::pair<const std::string, std::shared_ptr<GameSession>>& pair : sessions)
+        {
+            if (pair.second->BelongsToSession(senderIp, senderPort))
+            {
+                session = pair.second;
+                playerId = pair.second->GetPlayerIdByAddress(senderIp, senderPort);
+                break;
+            }
+        }
+    }
+
+    if (session == nullptr || playerId == -1)
+        return;
+
+    switch (type)
+    {
+    case TRANSFORM:
+        session->ProcessMovePacket(playerId, packet);
+        break;
+    case SHOOT:
+        session->ProcessShotPacket(playerId, packet);
+        break;
+    case PLAYER_TAUNT:
+        session->ProcessTauntPacket(playerId);
+        break;
+    default:
+        break;
+    }
+}
+
+void GameServer::CleanFinishedSessions()
+{
+    for (std::map<std::string, std::shared_ptr<GameSession>>::iterator it = sessions.begin(); it != sessions.end(); )
+    {
+        if (it->second->IsFinished())
+        {
+            std::cout << "Room deleted: " << it->first << std::endl;
+            it = sessions.erase(it);
+        }
+        else
+            ++it;
+    }
+}
---
+++ ServerUDP/Tutorial SFML/ProtocolData.h
@@ -0,0 +1,123 @@
+#pragma once
+
+#include <SFML/Network.hpp>
+#include <string>
+#include <vector>
+#include "PacketTypes.h"
+
+inline sf::Packet& operator<<(sf::Packet& packet, const sf::Vector2f& v)
+{
+    return packet << v.x << v.y;
+}
+inline sf::Packet& operator>>(sf::Packet& packet, sf::Vector2f& v)
+{
+    return packet >> v.x >> v.y;
+}
+
+struct LobbyPlayerInfo
+{
+    int playerId       = -1;
+    std::string username;
+    std::string ip;
+    unsigned short gamePort = 0;
+    bool isHost        = false;
+};
+
+inline sf::Packet& operator<<(sf::Packet& packet, const LobbyPlayerInfo& data)
+{
+    packet << data.playerId << data.username << data.ip << data.gamePort << data.isHost;
+    return packet;
+}
+inline sf::Packet& operator>>(sf::Packet& packet, LobbyPlayerInfo& data)
+{
+    packet >> data.playerId >> data.username >> data.ip >> data.gamePort >> data.isHost;
+    return packet;
+}
+
+// lo que manda el servidor TCP al UDP
+struct SessionStartData
+{
+    std::string roomId;
+    int playerCount = 0;
+    std::vector<LobbyPlayerInfo> players; // siempre 2 jugadores
+};
+
+sf::Packet& operator<<(sf::Packet& packet, const SessionStartData& data);
+sf::Packet& operator>>(sf::Packet& packet, SessionStartData& data);
+
+
+struct TransformData
+{
+    int packetId = 0;      // ID secuencial del paquete para reconciliacion
+    int dbId = -1;
+    int localPlayerId = -1;
+    float x = 0.f;
+    float y = 0.f;
+    bool flipped = false;
+};
+
+inline sf::Packet& operator<<(sf::Packet& packet, const TransformData& data)
+{
+    packet << data.packetId << data.dbId << data.localPlayerId << data.x << data.y << data.flipped;
+    return packet;
+}
+inline sf::Packet& operator>>(sf::Packet& packet, TransformData& data)
+{
+    packet >> data.packetId >> data.dbId >> data.localPlayerId >> data.x >> data.y >> data.flipped;
+    return packet;
+}
+
+// instanciar la bullet
+struct ShootReplicateData
+{
+    sf::Vector2f position;
+    bool flipped = false;
+};
+
+inline sf::Packet& operator<<(sf::Packet& packet, const ShootReplicateData& data)
+{
+    packet << data.position << data.flipped;
+    return packet;
+}
+inline sf::Packet& operator>>(sf::Packet& packet, ShootReplicateData& data)
+{
+    packet >> data.position >> data.flipped;
+    return packet;
+}
+
+struct PlayerHitData
+{
+    int targetPlayerId = -1;
+    int newHealth      = 0;
+    int newLifes       = 0;
+    sf::Vector2f respawnPosition;
+};
+
+inline sf::Packet& operator<<(sf::Packet& packet, const PlayerHitData& data)
+{
+    packet << data.targetPlayerId << data.newHealth << data.newLifes << data.respawnPosition;
+    return packet;
+}
+inline sf::Packet& operator>>(sf::Packet& packet, PlayerHitData& data)
+{
+    packet >> data.targetPlayerId >> data.newHealth >> data.newLifes >> data.respawnPosition;
+    return packet;
+}
+
+struct EndgameData
+{
+    int winnerPlayerId = -1;
+    int loserPlayerId  = -1;
+    bool cheating      = false;
+};
+
+inline sf::Packet& operator<<(sf::Packet& packet, const EndgameData& data)
+{
+    packet << data.winnerPlayerId << data.loserPlayerId << data.cheating;
+    return packet;
+}
+inline sf::Packet& operator>>(sf::Packet& packet, EndgameData& data)
+{
+    packet >> data.winnerPlayerId >> data.loserPlayerId >> data.cheating;
+    return packet;
+}
